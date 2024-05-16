5bddc85dd8cea25e6da3d32a8598a67dd98fbc18
Ary <alvarezaryam@gmail.com>
2024-05-16 00:59:13 -0400

    Task 4, fixed logic added new func

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,18 +3,17 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range(0, list_length):
+    for i in range(list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
-        except TypeError:
-            print("wrong type")
+        except (TypeError, ZeroDivisionError, IndexError) as error:
             div = 0
-        except ZeroDivisionError:
-            print("division by 0")
-            div = 0
-        except IndexError:
-            print("out of range")
-            div = 0
+            if isinstance(error, TypeError):
+                print("wrong type")
+            elif isinstance(error, ZeroDivisionError):
+                print("division by 0")
+            elif isinstance(error, IndexError):
+                print("out of range")
         finally:
             new_list.append(div)
     return new_list
