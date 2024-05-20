6e781c63aade05862cbb9584cdd6065df6a43de2
Ary <alvarezaryam@gmail.com>
2024-05-20 19:51:59 -0400

    Task 1, class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Below we define a class Square."""
+
+
+class Square:
+    """Here we represent a square."""
+
+    def __init__(self, size):
+        """Above were we initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
