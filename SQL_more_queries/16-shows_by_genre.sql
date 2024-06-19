6f091c17c80af98eba40b7f489c83ca1927f26fd
Ary <alvarezaryam@gmail.com>
2024-06-18 23:51:34 -0400

    More SQL Qrs

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,4 @@
+-- list all shows and genre linked to that SHOW
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name ASC;
